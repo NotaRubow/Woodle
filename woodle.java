454da6bb146a0e063ef04ce7262d6dd21d0faec1
rubown_wlhs <rubown@wlhs.wlwv.k12.or.us>
2024-12-03 15:00:04 -0800

    done for the day

---
+++ woodle.java
@@ -0,0 +1,8 @@
+import java.util.Scanner
+public class woodle {
+    public static void main(String[] args) {
+        String word = "apple";
+        Scanner input = new Scanner(System.in);
+        int attemps = 0;
+    }
+}
